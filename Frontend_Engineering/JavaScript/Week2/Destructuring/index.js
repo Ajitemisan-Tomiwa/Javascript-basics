81095ce3a4ba83c098773ad965eefc1df3274388
Ajitemisan Tomiwa <ajitemisantuoyo403@gmail.com>
2025-05-29 07:57:57 -0800

    solved destructuring

--- Frontend_Engineering/JavaScript/Week2/Destructuring/index.js
+++ Frontend_Engineering/JavaScript/Week2/Destructuring/index.js
@@ -1,61 +1,64 @@
 // Destructuring an Array
-let names = ["Adebayor", "Olubisi", "Ogunbiyi"]
+let names = ["Adebayor", "Olubisi", "Ogunbiyi"];
 
-let [firstName, middleName] = names
-
-console.log(firstName);
-console.log(middleName);
+// destructure the values of names into
+// new variables
+let = [firstName, lastName] = names;
+console.log(firstName, lastName);
+console.log(lastName);
 
 // Get the first name
-let [name1] = names
-console.log(name1) // Adebayor
+// assign it to another new variable
+let [name1] = names;
+console.log(name1); // Adebayor
 
 // Get the middle name
-let [, name2] = names
-console.log(name2) // Olubisi
+let [, name2] = names;
+console.log(name2);
+
+// or but will not work
+// comma has to be written to specify skipping of the next value
+// let [name2] = names;
+// console.log(name2); //'Adebayor'
 
 // Get the last name
-let [, , name3] = names
-console.log(name3) // Ogunbiyi
+let [, , name3] = names;
+console.log(name3); // Ogunbiyi
 
 // Use the rest operator to get the rest of the names
-let [name4, ...restOfNames] = names
-console.log(name4) // Adebayor
-console.log(restOfNames) // ["Olubisi", "Ogunbiyi"]
-
-
+let [name4, ...restOfNames] = names;
+console.log(name4); // Adebayor
+console.log(restOfNames); // ["Olubisi", "Ogunbiyi"]
 
 // Destructuring an Object
-let settings = { theme: "dark", fontSize: 20, fullScreen: true }
-let { theme, fontSize, fullScreen } = settings
-
+// unpacking values into new variables
+let settings = { theme: "dark", fontSize: 20, fullScreen: true };
+let { theme, fontSize, fullScreen } = settings;
 console.log(theme);
 console.log(fontSize);
 
 // Get only the theme, and assign it to a new variable
-let { theme: newTheme } = settings
+let { theme: newTheme } = settings;
 console.log(newTheme);
 
+// get the full screen and assign it to a new variable
+let { fullScreen: newFullScreen } = settings;
+console.log(newFullScreen);
+
 // Use the rest operator to get the rest of the settings
-let { theme: newTheme1, ...restOfSettings } = settings
-console.log(newTheme1) // dark
-console.log(restOfSettings) // { fontSize: 20, fullScreen: true }
-
-
+let { theme: anotherTheme, ...restOfSettings } = settings;
+console.log(theme); // dark
+console.log(restOfSettings); // { fontSize: 20, fullScreen: true }
 
 // Destructuring in a function
-function getUserInfo({ name, age }) {
-    console.log("My name is:", name)
-    console.log("My age is:" , age)
+function getUserInfo({ name: userName, age: userAge }) {
+  console.log(userName);
+  console.log(userAge);
 }
 
 const user = {
-    name: "John",
-    age: 23
-}
+  name: "John",
+  age: 23,
+};
 
-getUserInfo(user) // John 23
-
-
-
-
+getUserInfo(user); // John 23
